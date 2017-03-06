3c936d7ae86cb10fe88d5a9cd9b5244b0e8dc162
KyleReddeman <kyler7662@gmail.com>
2017-03-06 09:19:42 -0800

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public BoardManager boardScript;
+
+	private int level;
+	// Use this for initialization
+	void Awake () {
+		boardScript = GetComponent<BoardManager>();
+		level = 20;
+		InitGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InitGame() {
+		boardScript.SetupScene(level);
+	}
+}
